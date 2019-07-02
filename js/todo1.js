c432a6aea830010231c5c2348b74770e608d53b0
lilibethfabregas <fabregaslilibeth@gmail.com>
2019-07-02 23:50:27 +0800

    added style.css

--- js/todo1.js
+++ js/todo1.js
@@ -4,6 +4,8 @@ let tasks = JSON.parse(localStorage.getItem('tasks')) || []
 let delAllBtn = document.querySelector('#deleteAll')
 let deleteComp = document.querySelector('#deleteComp')
 let selectAll = document.querySelector('#selectAll')
+let btnContainer = document.querySelector('.btn-container')
+let deselectAll = document.querySelector('#deselectAll')
 
 // add tasks and persist them
 function addTask(e) {
@@ -21,6 +23,7 @@ function addTask(e) {
 	
 	this.reset()
 	displayTasks(tasks, taskList)
+	location.reload()
 }
 
 //display tasks on HTML side
@@ -29,7 +32,7 @@ function displayTasks(tasks = [], taskList) {
 		return `
 			<li class="list-group-item lists"> 
 				<input type="checkbox" class="col-lg-2" id="task${i}" data-index="${i}"  ${task.isDone ? "checked" : ""} />
-				<label for="task${i}" class="col-lg-8">${task.newTask}</label> 
+				<label for="task${i}" draggable="true" class="col-lg-8">${task.newTask}</label> 
 				<button class="delBtn" data-id="${i}">Delete</button>
 			</li>
 		` 
@@ -45,13 +48,13 @@ function toggleDone(e) {
 		//console.log(tasks[i].isDone)
 		localStorage.setItem('tasks', JSON.stringify(tasks))
  		displayTasks(tasks, taskList)
-	
+ 		location.reload()
 }
 
 
 //delete individual task
 document.querySelector('.todo-list').addEventListener('click', function(e) {
-	console.log(e.target)
+	//console.log(e.target)
 	if (e.target.className == "delBtn") {
 
 		if (!confirm('Are you sure you want to delete this?')) {
@@ -65,11 +68,14 @@ document.querySelector('.todo-list').addEventListener('click', function(e) {
  		displayTasks(tasks, taskList)
 	} 
 })
-	
+
+// event listener for adding tasks
 task.addEventListener('submit', addTask)
-//taskList.addEventListener('click', deleteTask)
+
+//event listener to change the task to done or not
 taskList.addEventListener('click', toggleDone)
 
+//delete all tasks
 delAllBtn.addEventListener('click', function() {
 	if (!confirm("Are you sure you want to delete all tasks")) {
 		return false
@@ -80,34 +86,54 @@ delAllBtn.addEventListener('click', function() {
 	location.reload()
 })
 
+//delete all completed tasks
 deleteComp.addEventListener('click', function(e) {
 	if (!confirm("Are you sure you want to delete all completed tasks?")) {
 		return false
 	}
-	//console.log(tasks)
-	if (tasks.every(task => task.isDone)) {
-		console.log("all is completed")
-	} else {
-		let pending = tasks.filter(e => e.isDone === false)
-		//console.log(pending)
-		localStorage.setItem('tasks', JSON.stringify(pending))
-		displayTasks(tasks, taskList)
-		location.reload()
-	}
 
-//	localStorage.removeItem('tasks')	
+	let pending = tasks.filter(e => e.isDone === false)
+	//console.log(pending)
+	localStorage.setItem('tasks', JSON.stringify(pending))
 	displayTasks(tasks, taskList)
+	location.reload()
 })
 
-// selectAll.addEventListener('click', function() {
-// 	//let i = e.target.dataset.index
-// 	//console.log(i)
-// 	tasks.isDone = !tasks.isDone 
-// 	//console.log(tasks[i].isDone)
-// 	localStorage.setItem('tasks', JSON.stringify(tasks))
-// 	displayTasks(tasks, taskList)
+//check all 
+selectAll.addEventListener('click', function() {
+	tasks.every(task => task.isDone = true);
+	localStorage.setItem('tasks', JSON.stringify(tasks))
+	displayTasks(tasks, taskList)
+	location.reload()
+})
 
-// })
+//uncheck all 
+deselectAll.addEventListener('click', function() {
+	tasks.forEach(task => task.isDone = false);
+	localStorage.setItem('tasks', JSON.stringify(tasks))
+	displayTasks(tasks, taskList)
+	location.reload()
+})
+
+//to hide the buttons when there is no items
+if (tasks.length === 0) {
+	btnContainer.style.opacity = 0
+} 
+ 
+let done = tasks.filter(e => e.isDone === true)
+if (done.length === 0) {
+	console.log('was')
+	deleteComp.style.display = 'none';
+	deselectAll.style.display = 'none';
+}
+
+let pending = tasks.filter(e => e.isDone === false)
+if (pending.length === 0) {
+	console.log('was')
+	deleteComp.style.display = 'none';
+	selectAll.style.display = 'none';
+}
+//tasks.filter
 
 
 displayTasks(tasks, taskList)
