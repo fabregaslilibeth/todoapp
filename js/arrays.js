535028d29e43250d173862cd8bd65116ac104fd1
lilibethfabregas <fabregaslilibeth@gmail.com>
2019-07-02 19:58:12 +0800

    initial commit

--- js/arrays.js
+++
@@ -1,6 +0,0 @@
-let arr = [1,4,3,2]
-let a = arr.reverse()
-console.log(a)
-let b = a.join(" ")
-console.log(b)
-
--- js/entries.js
+++
@@ -1,7 +0,0 @@
-let fruits = ["Banana", "Orange", "Apple", "Mango"];
-let values = fruits.entries()
-console.log(values)
-
-for (keys of values) {
-	document.querySelector('.demo').innerHTML += `<li> ${keys} </li>`
-}
